b7c2dbdd02bb7263e293d8f94d28bd6d908bb221
Rakshith J <132322840+Mainly-rakshith@users.noreply.github.com>
2025-11-02 16:27:10 -0600

    Remove example autonomous routine
    Removed the example autonomous routine from the code.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -3,18 +3,3 @@
  * Runs automatically when the robot is in autonomous mode during a match.
  */
 
-#include "main.h"
-
-// Example autonomous routine
-void autonomous() {
-  pros::Motor left_front(1);
-  pros::Motor right_front(2); // reverse one side
-
-  left_front.move_relative(500, 100);   // move forward 500 ticks
-  right_front.move_relative(500, 100);
-  pros::delay(2000); // wait for 2 seconds
-
-  left_front.move_relative(-500, 100);  // move back 500 ticks
-  right_front.move_relative(-500, 100);
-  pros::delay(2000);
-}
