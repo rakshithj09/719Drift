b4d620efeefbef8fbe0f4835beb070cb434f99fc
Rakshith J <rakshith.jk2@gmail.com>
2025-11-08 16:18:53 -0600

    Possible working auton

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -19,40 +19,46 @@
  extern pros::Motor front_top;
  extern pros::Motor back;
  
- static constexpr int RIGHT_SIGN = -1; // match driver control, right side reversed
+// Tunables
+static constexpr int DRIVE_POWER = 100;     // 0..127
+static constexpr int DRIVE_TIME_MS = 1200;  // how long to drive
+static constexpr int INTAKE_BTM = 127;      // intake in
+static constexpr int INTAKE_TOP = 127;
+static constexpr int INTAKE_REAR = -127;
 
- static void drive_move(int left_power, int right_power) {
-   LF.move(left_power);
-   LM.move(left_power);
-   LB.move(left_power);
- 
-   RF.move(RIGHT_SIGN * right_power);
-   RM.move(RIGHT_SIGN * right_power);
-   RB.move(RIGHT_SIGN * right_power);
- }
- 
- static void drive_brake() {
-   LF.brake(); LM.brake(); LB.brake();
-   RF.brake(); RM.brake(); RB.brake();
- }
- 
- // Intake helper
- static void intake_move(int bottom, int top, int rear) {
-   front_bottom.move(bottom);
-   front_top.move(top);
-   back.move(rear);
- }
+// Drive helper that matches opcontrol MotorGroup signs
+static void drive_move_forward(int power) {
+  // left:  {1, -2, -3}
+  LF.move( power);
+  LM.move(-power);
+  LB.move(-power);
+
+  // right: {-4, 5, 6}
+  RF.move(-power);
+  RM.move( power);
+  RB.move( power);
+}
+
+static void drive_brake() {
+  LF.brake(); LM.brake(); LB.brake();
+  RF.brake(); RM.brake(); RB.brake();
+}
+
+// Intake helper
+static void intake_move(int bottom, int top, int rear) {
+  front_bottom.move(bottom);
+  front_top.move(top);
+  back.move(rear);
+}
  
  void autonomous() {
-   // Intake in
-   intake_move(127, 127, -127);
- 
-   
-   // Drive forward
-   drive_move(100, 100);
-   pros::delay(1200); //Tells the wheel motors to stop after 1.2 seconds
- 
-   // Stop everything
-   drive_brake();
-   intake_move(0, 0, 0);
- }
+    // start intake and drive at the same time
+    intake_move(INTAKE_BTM, INTAKE_TOP, INTAKE_REAR);
+    drive_move_forward(DRIVE_POWER);
+  
+    pros::delay(DRIVE_TIME_MS);
+  
+    // stop
+    drive_brake();
+    intake_move(0, 0, 0);
+  }
